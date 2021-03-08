2d165eca1b9102f2193cfa9a684d2ef7da51901d
Prosper <prosper.nkongyum@orange.com>
2021-03-08 07:50:37 +0100

    Adding num_of_views field to Comic model

--- src/models/Comic.js
+++ src/models/Comic.js
@@ -11,22 +11,27 @@ const ComicSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  num_of_likes:{
+  num_of_views: {
     type: Number,
-    trim:true,
-    default:0
+    trim: true,
+    default: 0
+  },
+  num_of_likes: {
+    type: Number,
+    trim: true,
+    default: 0
   },
   cover: {
     type: String,
     trim: true
   },
   author: {
-     type: mongoose.Schema.ObjectId, 
-     ref: 'User' 
+    type: mongoose.Schema.ObjectId,
+    ref: "User"
   },
-  comic_type:{
-    type: mongoose.Schema.ObjectId, 
-    ref: 'ComicType' 
+  comic_type: {
+    type: mongoose.Schema.ObjectId,
+    ref: "ComicType"
   },
   updatedAt: {
     type: Date,
@@ -37,6 +42,5 @@ const ComicSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-
 
 module.exports = mongoose.model("Comic", ComicSchema);
